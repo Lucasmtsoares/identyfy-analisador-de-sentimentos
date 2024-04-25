e11ccf98b6c437c912d73133907df8431f7a2d74
Lucsmtsoares <lmsl3@aluno.ifal.edu.br>
2024-04-25 04:27:13 -0300

    teste

--- api/run.py
+++ api/run.py
@@ -13,31 +13,6 @@ app.config.from_object('config')
 def index():
     return render_template("basic.html")
 
-@app.route("/identificador-de-sentimentos", methods=['GET', 'POST'])
-def identificar_sentimento():
-    if request.method =='POST':
-        request_ = request.form['date']
-
-        sentiment = Sentiment(request_)
-        message = sentiment.get()
-        instance = sentiment.instance()
-        response = sentiment.execute()
-        return render_template('identyfy.html', response=response, message=message, instance=instance)
-    return render_template("identyfy.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/o-que-e-nltk")
-def explane_nltk():
-    return render_template("explane_nltk.html")
-
-@app.route("/quem-sou-eu")
-def my_description():
-    
-    return render_template("mydescription.html")
-
 
 
 
